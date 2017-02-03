522a1cd528d58c6066a75bba40f354a1b011fb87
Michael W. Delaney <michael.delaney@gmail.com>
2017-02-02 19:43:57 -0500

    Update class-acfci-template-loader.php

--- lib/class-acfci-template-loader.php
+++ lib/class-acfci-template-loader.php
@@ -22,7 +22,7 @@ class ACFCI_Template_Loader extends Gamajo_Template_Loader {
 	 * @since 1.0.0
 	 * @type string
 	 */
-	protected $theme_template_directory = 'ci-templates';
+	protected $theme_template_directory = 'templates/partials/cover-media';
  
 	/**
 	 * Reference to the root directory path of this plugin.
